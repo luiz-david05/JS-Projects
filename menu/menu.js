ae4c60ef4a82f6c9d4b8e69433b95caa53c8abd8
luiz-david05 <decd393@gmail.com>
2023-06-05 12:10:58 -0300

    atualização no menu

--- menu/menu.js
+++ menu/menu.js
@@ -3,11 +3,19 @@ import {
     get_positive_number, 
     input} from "./entrada_utils.js";
 import {
-     criar_vetor_vazio,
-     mostrar_vetor, 
-     preencher_vetor_automaticamente, 
-     preencher_vetor_com_valor_padrao, 
-     preencher_vetor_manualmente} from './menu_utils.js'
+    criar_vetor_vazio,
+    elevar_elementos_a_n,
+    mapear_vetor,
+    mostrar_vetor, 
+    preencher_vetor_automaticamente, 
+    preencher_vetor_com_valor_padrao, 
+    preencher_vetor_manualmente,
+    filtrar_vetor,
+    eh_positivo, eh_negativo, eh_nulo,
+    reduzir_vetor,
+    somatorio,
+    media_vetor, mediana_vetor,
+    sortear_elemento} from './menu_utils.js'
 
 
 function main() {
@@ -19,6 +27,7 @@ function main() {
         if (opcao === 1) {
             vetor = criar_vetor_vazio()
             vetor = preencher_vetor_com_valor_padrao(vetor)
+
             console.log("\nVetor criado com sucesso!")
             continuar()
             menu()
@@ -27,6 +36,7 @@ function main() {
         else if (opcao === 2) {
             vetor = criar_vetor_vazio()
             vetor = preencher_vetor_manualmente(vetor)
+
             console.log("\nVetor preenchido com sucesso!")
             continuar()
             menu()
@@ -35,6 +45,7 @@ function main() {
         else if (opcao === 3) {
             vetor = criar_vetor_vazio()
             vetor = preencher_vetor_automaticamente(vetor)
+
             console.log("\nVetor preenchido com sucesso!")
             continuar()
             menu()
@@ -42,6 +53,86 @@ function main() {
         }
         else if (opcao === 4) {
             mostrar_vetor(vetor)
+            continuar()
+            menu()
+            opcao = opcao_valida()
+        }
+        else if (opcao === 5) {
+            const n = get_number("valor de n: ")
+            vetor = mapear_vetor(vetor, elevar_elementos_a_n(n))
+
+            console.log("\nVetor transformado com sucesso!")
+            continuar()
+            menu()
+            opcao = opcao_valida()
+        }
+        else if (opcao === 6) {
+            const positivos = filtrar_vetor(vetor, eh_positivo)
+            const negativos = filtrar_vetor(vetor, eh_negativo)
+            const nulos = filtrar_vetor(vetor, eh_nulo)
+            
+            console.log(`\nQuantidade de elementos positivos: ${positivos.length}`)
+            console.log(`Quantidade de elementos negativos: ${negativos.length}`)
+            console.log(`Quantidade de elementos nulos: ${nulos.length}`)
+            continuar()
+            menu()
+            opcao = opcao_valida()
+        }
+        else if (opcao === 7) {
+            const positivos = filtrar_vetor(vetor, eh_positivo)
+            const negativos = filtrar_vetor(vetor, eh_negativo)
+            
+            const somatorio_vetor = reduzir_vetor(vetor, somatorio)
+            const somatorio_positivos = reduzir_vetor(positivos, somatorio)
+            const somatorio_negativos = reduzir_vetor(negativos, somatorio)
+
+            console.log(`\nSomatório de todos os elementos do vetor: ${somatorio_vetor}`)
+            console.log(`Somatório de todos os elementos positivos do vetor: ${somatorio_positivos}`)
+            console.log(`Somatório de todos os elementos negativos do vetor: ${somatorio_negativos}`)
+            continuar()
+            menu()
+            opcao = opcao_valida()
+        }
+        else if (opcao === 8) {
+            const positivos = filtrar_vetor(vetor, eh_positivo)
+            const negativos = filtrar_vetor(vetor, eh_negativo)
+
+            const media = media_vetor(vetor)
+            const mediana = mediana_vetor(vetor)
+            const media_positivos = media_vetor(positivos)
+            const mediana_positivos = mediana_vetor(positivos)
+            const media_negativos = media_vetor(negativos)
+            const mediana_negativos = mediana_vetor(negativos)
+
+            console.log(`\nMédia de todos os elementos do vetor: ${media}`)
+            console.log(`Mediana de todos os elementos do vetor: ${mediana}`)
+            console.log(`Média de todos os elementos positivos do vetor: ${media_positivos}`)
+            console.log(`Mediana de todos os elementos positivos do vetor: ${mediana_positivos}`)
+            console.log(`Média de todos os elementos negativos do vetor: ${media_negativos}`)
+            console.log(`Mediana de todos os elementos negativos do vetor: ${mediana_negativos}`)
+            continuar()
+            menu()
+            opcao = opcao_valida()
+        }
+        else if (opcao === 9) {
+            const maior = Math.max(...vetor)
+            const menor = Math.min(...vetor)
+
+            console.log(`\nMaior elemento do vetor: ${maior}`)
+            console.log(`Menor elemento do vetor: ${menor}`)
+            continuar()
+            menu()
+            opcao = opcao_valida()
+        }
+        else if (opcao === 10 ) {
+            const positivos = filtrar_vetor(vetor, eh_positivo)
+            const negativos = filtrar_vetor(vetor, eh_negativo)
+
+            const elemento_positivo = sortear_elemento(positivos)
+            const elemento_negativo = sortear_elemento(negativos)
+
+            console.log(`\nElemento positivo sorteado: ${elemento_positivo}`)
+            console.log(`Elemento negativo sorteado: ${elemento_negativo}`)
             continuar()
             menu()
             opcao = opcao_valida()
@@ -56,6 +147,12 @@ function menu() {
     console.log("2 - Criar vetor e preencher manualmente item a item")
     console.log("3 - Criar vetor e preencher automaticamente")
     console.log("4 - Mostrar vetor")
+    console.log("5 - Transformar vetor: elevar elementos a potência n")
+    console.log("6 - Contar elementos positivos, negativos e nulos")
+    console.log("7 - Somar todos os elementos do vetor, apenas os positivos e \napenas os negativos (separadamente)")
+    console.log("8 - Exibir média e mediana de: todos os elementos do vetor, apenas os positivos e \napenas os negativos (separadamente)")
+    console.log("9 - Exibir maior e menor elemento do vetor")
+    console.log("10 - Sortear dois valores do vetor: um positivo e outro negativo")
     console.log("0 - sair")
 }
 
@@ -63,7 +160,7 @@ function menu() {
 function opcao_valida() {
     let opcao = get_number("\nDigite uma opção: ")
 
-    if (opcao < 0 || opcao > 4) {
+    if (opcao < 0 || opcao > 10) {
         console.log("Opção inválida, tente novamente.")
         opcao = opcao_valida()
     }
--- menu/menu_utils.js
+++ menu/menu_utils.js
@@ -46,3 +46,124 @@ export const mostrar_vetor = (vetor) => {
     console.log("\nVetor:")
     console.table(vetor)
 }
+
+
+export const mapear_vetor = (vetor, funcao) => {
+    const vetor_mapeado = new Array(vetor.length)
+
+    for (let i = 0; i < vetor.length; i++) {
+        vetor_mapeado[i] = funcao(vetor[i])
+    }
+
+    return vetor_mapeado
+}
+
+
+export const elevar_elementos_a_n = (n) => {
+    return (elemento) => {
+        return elemento ** n
+    }
+}
+
+
+const adicionar_elemento = (vetor, elemento) => {   
+    vetor[vetor.length] = elemento
+
+    return vetor
+}
+
+
+export const filtrar_vetor = (vetor, funcao) => {
+    const vetor_filtrado = []
+
+    for (const elemento in vetor) {
+        if (funcao(vetor[elemento])) {
+            adicionar_elemento(vetor_filtrado, vetor[elemento])
+        }
+    }
+    return vetor_filtrado
+}
+
+
+export const eh_positivo = (n) => n > 0
+export const eh_negativo = (n) => n < 0
+export const eh_nulo = (n) => n === 0
+const eh_par = (n) => n % 2 === 0
+
+
+export const reduzir_vetor = (vetor, funcao) => {
+    let valor_reduzido = 0
+
+    for (let i = 0; i < vetor.length; i++) {
+        valor_reduzido = funcao(valor_reduzido, vetor[i])
+    }
+
+    return valor_reduzido
+}
+
+
+export const somatorio = (soma, elemento) => soma + elemento
+
+
+export const media_vetor = (vetor) => {
+    const somatorio_vetor = reduzir_vetor(vetor, somatorio)
+
+    return somatorio_vetor / vetor.length
+}
+
+
+export const ordenar_vetor = (vetor) => {
+    for (let i = 0; i < vetor.length; i++) {
+        for (let j = 0; j < vetor.length - 1; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                const aux = vetor[j]
+                vetor[j] = vetor[j + 1]
+                vetor[j + 1] = aux
+            }
+        }
+    }
+
+    return vetor
+}
+
+
+export const mediana_vetor = (vetor) => {
+    const vetor_ordenado = ordenar_vetor(vetor)
+    let mediana
+
+    if (eh_par(vetor_ordenado.length)) {
+        const indice_meio = vetor_ordenado.length / 2
+
+        mediana = (vetor_ordenado[indice_meio - 1] + vetor_ordenado[indice_meio]) / 2
+    } else {
+        const indice_meio = Math.floor(vetor_ordenado.length / 2)
+
+        mediana = vetor_ordenado[indice_meio]
+    }
+
+    return mediana
+}
+
+
+export const sortear_elemento = (vetor) => {
+    const indice_sorteado = Math.floor(Math.random() * vetor.length)
+
+    return vetor[indice_sorteado]
+}
+
+
+// const teste = () => {
+//     const vetor = [5, 2, 9, -1, 7, 6, -3, -2]
+    
+//     const positivos = filtrar_vetor(vetor, eh_positivo)
+//     const negativos = filtrar_vetor(vetor, eh_negativo)
+    
+//     const elemento_positivo = sortear_elemento(positivos)
+//     const elemento_negativo = sortear_elemento(negativos)
+
+//     console.log(elemento_positivo)
+//     console.log(elemento_negativo)
+// }
+
+
+// teste()
